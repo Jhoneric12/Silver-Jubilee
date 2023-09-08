a7ebedac5775a89bddab6f3df1fce3f635e57b4b
Jhon Eric <jhonerix23@gmail.com>
2023-09-08 22:29:15 +0800

    modified container width in event images

--- app.js
+++ app.js
@@ -7,11 +7,14 @@
  window.onscroll = function() {scrollFunction()};
 
  function scrollFunction() {
- if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-     mybutton.style.display = "block";
- } else {
-     mybutton.style.display = "none";
- }
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) 
+    {
+        mybutton.style.display = "block";
+    } 
+    else 
+    {
+        mybutton.style.display = "none";
+    }
  }
 
  // When back button is clicked
